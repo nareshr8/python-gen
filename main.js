d7053f9559ea634a7730965c32fdc6c52e05ceb8
Naresh Rangan <n0r033u@walmartlabs.com>
2022-05-03 18:34:01 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,43 @@
+define([
+    'base/js/namespace',
+    'base/js/events'
+    ], function(Jupyter, events) {
+
+        // Adds a cell above current cell (will be top if no cells)
+        var create_python = function() {
+	if(Jupyter.notebook.get_cell(-1).is_generated){
+    Jupyter.notebook.delete_cell(-1);
+}
+            Jupyter.notebook.insert_cell_at_bottom('code').set_text(`from nbdev.export import notebook2script 
+notebook2script()`);
+            Jupyter.notebook.execute_cells([-1]);
+	setTimeout(function(){
+var output = Jupyter.notebook.get_cell(-1).output_area.outputs[0].text;
+	output=output.replaceAll('\n','\n\n');
+	Jupyter.notebook.delete_cell(-1);
+mk_val=Jupyter.notebook.insert_cell_at_bottom('markdown');
+mk_val.set_text(output);
+mk_val.is_generated=true;
+            Jupyter.notebook.execute_cells([-1]);
+}, 2000);
+            // 
+            // Button to add default cell
+      }
+
+      var defaultCellButton = function () {
+        Jupyter.toolbar.add_buttons_group([
+            Jupyter.keyboard_manager.actions.register ({
+                'help': 'Convert to Python',
+                'icon' : 'fa-code',
+                'handler': create_python
+            }, 'create-python-file', 'python File')
+        ])
+    };
+    // Run on start
+    function load_ipython_extension() {
+        defaultCellButton();
+    }
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Ipynb to Python
+
+Extension for converting IPython notebook to Python file for nbdev users. Note that nbdev is required for the conversion to work.
